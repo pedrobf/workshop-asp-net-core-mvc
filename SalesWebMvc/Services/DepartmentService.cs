60e73880b48dd56c64165cf0e149c13bf9e4ed4a
Pedro Henrique Bertoni Ferraro <pedrobferraro@gmail.com>
2019-01-14 14:24:13 -0200

    Async operations

--- SalesWebMvc/Services/DepartmentService.cs
+++ SalesWebMvc/Services/DepartmentService.cs
@@ -1,4 +1,5 @@
-﻿using SalesWebMvc.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,9 +15,9 @@ namespace SalesWebMvc.Services
         {
             _context = context;
         }
-        public List<Department> FindAll()
+        public async Task<List<Department>> FindAllAsync()
         {
-            return _context.Department.OrderBy(obj => obj.Name).ToList();
+            return await _context.Department.OrderBy(obj => obj.Name).ToListAsync();
         }
     }
 }
